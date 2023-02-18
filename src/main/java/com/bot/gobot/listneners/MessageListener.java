3e264044d53160c9958ef060a96f8122c7933e10
sebastian waltilla <sebastianwaltilla@hotmail.com>
2023-02-18 19:40:08 +0100

    only allow list of members

--- src/main/java/com/bot/gobot/listneners/MessageListener.java
+++ src/main/java/com/bot/gobot/listneners/MessageListener.java
@@ -3,26 +3,35 @@ package com.bot.gobot.listneners;
 import discord4j.core.object.entity.Message;
 import reactor.core.publisher.Mono;
 
+import java.util.Arrays;
+import java.util.List;
+
 public abstract class MessageListener {
 
-   private String author = "UNKNOWN";
 
-   public Mono<Void> processMessage(final Message eventMessage){
-       return Mono.just(eventMessage)
-          .filter(message -> {
-                   final Boolean isNotBot = message.getAuthor()
-                           .map(user -> !user.isBot())
-                           .orElse(false);
 
-                   if(isNotBot){
-                       message.getAuthor().ifPresent(user -> author = user.getUsername());
-                   }
-                   return isNotBot;
-               })
-               .flatMap(Message::getChannel)
-               .flatMap(channel -> channel.createMessage("echo " + author))
-               .then();
+    public Mono<Void> processMessage(final Message eventMessage){
 
-       // Skriv om för att ta in message och sedan skicka ut bilden.
-   }
+        List<String> listOfOkUsers = Arrays.asList("Timmyonetoe", "BooGrim");
+
+        return Mono.just(eventMessage)
+                .filter(message -> {
+                    final Boolean canPlay = message.getAuthor()
+                            .map(user -> !user.isBot())
+                            .orElse(false);
+
+                    if(canPlay){
+                        message.getAuthor().ifPresent(listOfOkUsers::contains);
+                    }
+                    return canPlay;
+                })
+                .flatMap(Message::getChannel)
+                .flatMap(channel -> channel.createMessage("Ja dig kan man lita på!"))
+                .then();
+
+        // Skriv om för att ta in message och sedan skicka ut bilden.
+    }
+
+
+
 }

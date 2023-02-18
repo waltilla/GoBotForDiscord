fc7c247b6dc0ee954ad38413deaf9eaef357b57b
sebastian waltilla <sebastianwaltilla@hotmail.com>
2023-02-18 21:12:09 +0100

    message listner can now return the go board

--- src/main/java/com/bot/gobot/listneners/MessageListener.java
+++ src/main/java/com/bot/gobot/listneners/MessageListener.java
@@ -1,18 +1,33 @@
 package com.bot.gobot.listneners;
 
 import discord4j.core.object.entity.Message;
+import discord4j.core.spec.MessageCreateSpec;
 import reactor.core.publisher.Mono;
 
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
 
 public abstract class MessageListener {
 
 
+    public Mono<Void> processMessage(final Message eventMessage) {
 
-    public Mono<Void> processMessage(final Message eventMessage){
+        List<String> listOfUsers = Arrays.asList("Timmyonetoe", "BooGrim");
+        InputStream targetStream = null;
 
-        List<String> listOfOkUsers = Arrays.asList("Timmyonetoe", "BooGrim");
+
+        try {
+            targetStream =
+                    new DataInputStream(new FileInputStream("src/main/resources/Output.png"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        InputStream finalTargetStream = targetStream;
 
         return Mono.just(eventMessage)
                 .filter(message -> {
@@ -20,18 +35,20 @@ public abstract class MessageListener {
                             .map(user -> !user.isBot())
                             .orElse(false);
 
-                    if(canPlay){
-                        message.getAuthor().ifPresent(listOfOkUsers::contains);
+                    if (canPlay) {
+                        message.getAuthor().ifPresent(listOfUsers::contains);
                     }
                     return canPlay;
                 })
                 .flatMap(Message::getChannel)
-                .flatMap(channel -> channel.createMessage("Ja dig kan man lita på!"))
+                .flatMap(channel -> channel.createMessage(MessageCreateSpec.builder()
+                        .content("Your move: " + channel.getLastMessage().toString())
+                        .addFile("src/main/resources/Output.png", finalTargetStream)
+                        .build()))
                 .then();
 
         // Skriv om för att ta in message och sedan skicka ut bilden.
     }
-
 
 
 }

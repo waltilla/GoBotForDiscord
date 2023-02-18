e947923bc6d810670b321138c1803331ed3a26b3
sebastian waltilla <sebastianwaltilla@hotmail.com>
2023-02-18 13:42:59 +0100

    update with AppConfig

---
+++ src/main/java/com/bot/gobot/config/AppConfig.java
@@ -0,0 +1,34 @@
+package com.bot.gobot.config;
+
+import com.bot.gobot.listneners.EventListener;
+import discord4j.core.DiscordClientBuilder;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.Event;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
+public class AppConfig {
+
+
+    @Value("${discord.token}")
+    private String token;
+
+    @Bean
+    public <T extends Event>GatewayDiscordClient gatewayDiscordClient(final List<EventListener> eventListeners){
+
+        final GatewayDiscordClient client = DiscordClientBuilder.create(token)
+                .build().login().block();
+
+        for (final EventListener<T> listener:eventListeners){
+            client.on(listener.getEventType())
+                    .flatMap(listener::execute)
+                    .onErrorResume(listener::handleError)
+                    .subscribe();
+        }
+
+        return client;
+
+    }
+}
---
+++ src/main/java/com/bot/gobot/listneners/EventListener.java
@@ -0,0 +1,17 @@
+package com.bot.gobot.listneners;
+
+import discord4j.core.event.domain.Event;
+import reactor.core.publisher.Mono;
+
+public interface EventListener<T extends Event> {
+
+    Class<T> getEventType();
+
+    Mono<Void> execute(T event);
+
+    default Mono<Void> handleError(final Throwable error){
+        error.printStackTrace();
+
+        return Mono.empty();
+    }
+}

0e477b6a08397915d270d90a7045ec53a98368ff
sebastian waltilla <sebastianwaltilla@hotmail.com>
2023-02-26 21:12:01 +0100

    prepare for johan to solve the logic

---
+++ src/main/java/com/bot/gobot/go/LogicStone.java
@@ -0,0 +1,12 @@
+package com.bot.gobot.go;
+
+import java.util.List;
+
+public class LogicStone {
+    public String color;
+    public int positionX;
+    public int positionY;
+    int liberties;
+    List<Stone> neighbours;
+    
+}

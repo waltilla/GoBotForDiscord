26e0aa23886efe8e16c1caf47b905b1c38f6c15e
sebastian waltilla <sebastianwaltilla@hotmail.com>
2023-04-23 18:54:05 +0200

    cleanup message listner

---
+++ src/main/java/com/bot/gobot/config/AllowedPlayersConfig.java
@@ -0,0 +1,17 @@
+package com.bot.gobot.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "allowed")
+public class AllowedPlayersConfig {
+    private List<String> players;
+
+}
--- src/main/java/com/bot/gobot/listneners/MessageListener.java
+++ src/main/java/com/bot/gobot/listneners/MessageListener.java
@@ -1,117 +1,102 @@
 package com.bot.gobot.listneners;
 
+import com.bot.gobot.config.AllowedPlayersConfig;
 import com.bot.gobot.go.Game;
 import com.bot.gobot.go.Goban;
 import com.bot.gobot.img.CreatePixelArrayFromKifu;
 import com.bot.gobot.img.ImageFromIntArray;
 import discord4j.core.object.entity.Message;
 import discord4j.core.spec.MessageCreateSpec;
+import org.springframework.beans.factory.annotation.Autowired;
 import reactor.core.publisher.Mono;
 
-import java.io.*;
-import java.util.Arrays;
-import java.util.List;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public abstract class MessageListener {
 
     Game game;
-    String info = "";
+    String messageToPlayer = "";
+
+    @Autowired
+    AllowedPlayersConfig allowedPlayers;
 
     public MessageListener() {
         game = new Game();
     }
 
     public Mono<Void> processMessage(final Message eventMessage) {
-        info = "";
-        List<String> listOfUsers = Arrays.asList("Timmyonetoe", "BooGrim", "Vickypod", "Bullenjullen");
-        InputStream targetStream = null;
-        String filePathToGobanToPrint = System.getProperty("java.io.tmpdir") + "Output.png";
-        System.out.println(filePathToGobanToPrint);
-        try {
-            targetStream =
-                    new DataInputStream(new FileInputStream(filePathToGobanToPrint));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+
+        // Check if message is from author in allowed list of players in application.yml
+        if (allowedPlayers.getPlayers().stream().filter(s -> s.equals(eventMessage.getAuthor().get().getUsername())).toList().size() == 1
+                && eventMessage.getData().content().charAt(0) == '&') {
+            return playAMove(eventMessage);
         }
 
-        InputStream finalTargetStream = targetStream;
+        return Mono.empty();
+    }
 
+    private Mono<Void> playAMove(Message eventMessage) {
+
+        String filePathToGobanToPrint = System.getProperty("java.io.tmpdir") + "Output.png";
+        InputStream finalTargetStream = targetStream(filePathToGobanToPrint);
 
         return Mono.just(eventMessage)
                 .filter(message -> {
-                    final Boolean canPlay = message.getAuthor()
-                            .map(user -> !user.isBot())
-                            .orElse(false);
-
-                    System.out.println(message.getData().content());
-
-
-                    if (canPlay) {
-
-                        message.getAuthor().ifPresent(listOfUsers::contains);
-                        String messageFromPlayer = eventMessage.getContent();
-                        if (messageFromPlayer.charAt(0) != '&' && eventMessage.getData().channelId().asString().equals("1075151561401061386")) {
-                            return false;
+                        String messageFromChat = eventMessage.getContent().substring(1);
+                        if (messageFromChat.equals("info")) {
+                            messageToPlayer = info();
+                        } else if (messageFromChat.equals("new")) {
+                            game = new Game();
+                        } else if (messageFromChat.equals("undo")) {
+                            game.undo();
+                        } else {
+                            makeMove(eventMessage, messageFromChat);
                         }
-
-                        // The bot responds "only" if its starts with &
-                        if (messageFromPlayer.charAt(0) == '&') {
-
-                            String messageFromChat = messageFromPlayer.substring(1);
-
-                            if (messageFromChat.equals("info")) {
-                                info = info();
-                            }
-
-                            else if (messageFromChat.equals("new")) {
-                                game = new Game();
-                            }
-
-                            else if (messageFromChat.equals("undo")) {
-                                game.undo();
-                            } else {
-                                try {
-                                    game.addMove(eventMessage.getAuthor().get().getUsername(), messageFromChat);
-                                } catch (Exception e) {
-                                    System.out.println("prutt");
-                                }
-                            }
-
-
-
-                            ImageFromIntArray.generate(
-                                    CreatePixelArrayFromKifu.
-                                            createImageOfGobanFromKifu(Goban.getCleanGoban(), game.getKifu()));
-
-                        }
-                    }
-
-
-                    return canPlay;
+                    return true;
                 })
                 .flatMap(Message::getChannel)
                 .flatMap(channel ->
-
-
                         channel.createMessage(MessageCreateSpec.builder()
-                                .content("Your prompt: " + eventMessage.getContent() + "\n" + info)
+                                .content(messageToPlayer)
                                 .addFile(filePathToGobanToPrint, finalTargetStream)
                                 .build()))
                 .then();
-        // Skriv om för att ta in message och sedan skicka ut bilden.
+    }
+
+    private void makeMove(Message eventMessage, String messageFromChat) {
+        try {
+            game.addMove(eventMessage.getAuthor().get().getUsername(), messageFromChat);
+            messageToPlayer = messageFromChat;
+            ImageFromIntArray.generate(
+                    CreatePixelArrayFromKifu.
+                            createImageOfGobanFromKifu(Goban.getCleanGoban(), game.getKifu()));
+        } catch (Exception e) {
+            System.out.println("Error...");
+        }
+    }
+
+    private InputStream targetStream(String Path) {
+
+        try {
+            return new DataInputStream(new FileInputStream(Path));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public String info() {
-        return  """
+        return """
                 You have the following options: 
                 &new == new game
                 &12-12 == putting a stone on position 12x12
                 &undo == undo last move
-                
+                                
                 thats it. Have fun! 
                                 
                 """;
     }
-
-
 }
